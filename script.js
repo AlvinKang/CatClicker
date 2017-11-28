e708c99f2df0e7c4726adc33c0612438ea6f8a79
Alvin Kang <oneinallin1@gmail.com>
2017-11-27 17:25:46 -0800

    Adapt to changed requirement where there are two cats instead of 1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var counter = 0;
+var counter1 = 0;
+var counter2 = 0;
 
-$('#cat-pic').click(function() {
-	counter++;
-	$('#counter').text(counter);
+var catName1 = "George";
+var catName2 = "Piccolo";
+
+$('#cat-name1').text(catName1);
+$('#cat-name2').text(catName2);
+
+$('#cat-pic1').click(function() {
+	counter1++;
+	$('#counter1').text(counter1);
+});
+
+$('#cat-pic2').click(function() {
+	counter2++;
+	$('#counter2').text(counter2);
 });
